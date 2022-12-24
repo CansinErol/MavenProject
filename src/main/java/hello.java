98e214517de3b680e17355b8d3306bf3afd4e621
cansin-erol <cansineroll@gmail.com>
2022-12-24 21:46:39 +0300

    added more hello feature

--- src/main/java/hello.java
+++ src/main/java/hello.java
@@ -1,6 +1,8 @@
 public class hello {
     public static void main(String[] args) {
         System.out.println("hello");
-        System.out.println("developing everthing in the future");
+        System.out.println("developing everything in the future");
+        System.out.println("local Repo is already published to github");
+        System.out.println("do more everytime");
     }
 }
